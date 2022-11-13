0224a7799029c4e3cd767e1a86916d85519ec951
joeyst <joeystenbeck@gmail.com>
2022-11-13 15:48:45 -0800

    Remove dead code.

--- main.rb
+++ main.rb
@@ -5,9 +5,6 @@ require_relative 'bike'
 bike = Bike.new(1, :pink, 99.99)
 
 bike.add_cargos(:apples, :water, :repair_kit)
-# bike.add_cargo(:apples)
-# bike.add_cargo(:water)
-# bike.add_cargo(:repair_kit)
 
 puts "Space for #{bike.pannier_remaining_capacity} items left."
 
