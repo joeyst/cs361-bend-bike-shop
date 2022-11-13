588ff0904247efe9c7e842fb5ac374db2af43239
joeyst <joeystenbeck@gmail.com>
2022-11-13 15:53:12 -0800

    Revert to original code.

--- bike.rb
+++ bike.rb
@@ -2,83 +2,38 @@
 
 class Bike
 
-  STANDARD_WEIGHT_LBS = 200 # lbs
+  STANDARD_WEIGHT = 200 # lbs
   MAX_CARGO_ITEMS = 10
 
-  attr_accessor :color, :weight, :pannier, :rental_data
+  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT_LBS, rented = false)
-    @rental_data = recordify(id, price, rented)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+    @id = id
     @color = color
+    @price = price
     @weight = weight
-    @pannier = Pannier.new(MAX_CARGO_ITEMS)
-  end
-
-  RentalData = Struct.new(:id, :price, :rented)
-  def recordify(id, price, rented)
-    RentalData.new(id, price, rented)
+    @rented = rented
+    @cargo_contents = []
   end
 
   def rent!
-    rental_data.rented = true
+    self.rented = true
   end
 
-  def add_cargos(*items)
-    items.each {|item| add_cargo(item)}
-  end
-  
   def add_cargo(item)
-    pannier.add(item)
+    self.cargo_contents << item
   end
 
   def remove_cargo(item)
-    pannier.remove(item)
+    self.cargo_contents.remove(item)
   end
 
   def pannier_capacity
-    pannier.capacity
+    MAX_CARGO_ITEMS
   end
 
   def pannier_remaining_capacity
-    pannier.remaining_capacity
+    MAX_CARGO_ITEMS - self.cargo_contents.size
   end
 
-  def cargo_content_size
-    pannier.size
-  end
-
-end
-
-class Pannier
-  attr_accessor :contents, :capacity 
-
-  def initialize(capacity, contents=[])
-    @capacity = capacity
-    @contents = contents
-  end
-
-  def add(item)
-    contents << Item.new(item)
-  end
-
-  def remove(item)
-    contents.remove(Item.new(item))
-  end
-
-  def remaining_capacity
-    capacity - size
-  end
-
-  def size
-    contents.size
-  end
-  
-end
-
-class Item
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
 end
