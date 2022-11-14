4934b87ef88c4f8aac642286fe1b7ddaf5ebec14
joeyst <joeystenbeck@gmail.com>
2022-11-14 10:50:40 -0800

    Remove passing Bike object to child.

--- bike.rb
+++ bike.rb
@@ -12,7 +12,7 @@ class Bike
     @color = color
     @price = price
     @weight = STANDARD_WEIGHT_LBS
-    @luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, extra_items, self)
+    @luggage = Luggage.new(Luggage::DEFAULT_MAX_CAPACITY, extra_items)
   end
 
 end
